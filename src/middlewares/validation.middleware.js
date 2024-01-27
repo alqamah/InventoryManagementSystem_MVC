cdaf8994ace642a59feefbec856530f66e53283d
alqamah <alqama.hasnain@gmail.com>
2024-01-27 16:43:47 +0530

    Adding the dir

---
+++ src/middlewares/validation.middleware.js
@@ -0,0 +1,34 @@
+import {body, validationResult} from 'express-validator'; //imports the body of request
+
+const validateRequest = async (req, res, next) =>{
+  console.log('Inside validateRequest',req.body);
+   //Step 1. Setup rules for validation
+  const rules = [
+    body('name').notEmpty().withMessage('Name must be at least 5 characters long'),
+    body('price').isFloat({min:1}).withMessage('Price must be a positive value'),
+    body('imageUrl').isURL().withMessage('URL is invalid')
+  ];
+
+  //Step 2. Run the rules
+  /**
+   * Runs validation rules asynchronously using Promise.all.
+   * Maps array of rules to array of promises from rule.run(), 
+   * awaits all promises to complete, 
+  */
+  await Promise.all( 
+    rules.map(rule => rule.run(req));
+  );
+
+  //Step 3. Get the validation errors
+  var errors = validationResult(req);
+  console.log('errors',errors.array());
+  if (!errors.isEmpty()) {
+    return res.render('new-product', {
+      errorMessage: errors.array()[0].msg
+    });
+  }
+  next();
+}
+
+export default validateRequest;
+//To use export default: Hoisted declaration; Class; Assignment declaration
